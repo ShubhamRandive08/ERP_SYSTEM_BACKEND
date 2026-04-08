6aa682168460b9869222c897dad6880024fada2c
Shubham Balavant Randive <srandive245@gmail.com>
2026-04-08 17:43:33 +0530

    add new cofing

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,10 @@
-const { Pool } = require("pg");
-
+const { Pool } = require('pg');
 const pool = new Pool({
-  user: "erp_system_cbtp_user",
-  host: "dpg-d79ne10gjchc73fnnqdg-a.oregon-postgres.render.com",
-  database: "erp_system_cbtp",
-  password: "as5OUSqEn7f9Ht1JEZIJylbHZsExbiYp",
-  port: 5432,
+  connectionString: 'postgresql://erp_system_cbtp_user:as5OUSqEn7f9Ht1JEZIJylbHZsExbiYp@dpg-d79ne10gjchc73fnnqdg-a.oregon-postgres.render.com/erp_system_cbtp',
+  ssl: { rejectUnauthorized: false }
 });
-
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) console.error(err);
+  else console.log('Connected:', res.rows[0]);
+});
 module.exports = pool;
