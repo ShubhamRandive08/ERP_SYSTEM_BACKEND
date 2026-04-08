2711dcf4d33015fc3d0aeaadc33bb0b52b98ccd2
Shubham Balavant Randive <srandive245@gmail.com>
2026-04-08 16:46:37 +0530

    add configuration of db

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "erp_system",
-  password: "Kingsr@08",
+  user: "erp_system_cbtp_user",
+  host: "dpg-d79ne10gjchc73fnnqdg-a.oregon-postgres.render.com",
+  database: "erp_system_cbtp",
+  password: "as5OUSqEn7f9Ht1JEZIJylbHZsExbiYp",
   port: 5432,
 });
 
